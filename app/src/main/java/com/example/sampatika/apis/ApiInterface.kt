19ee83f01f0d37f42210a773b1c247bc0822fb38
Vishal Verma <vishalpbh022@gmail.com>
2025-07-04 07:10:13 +0530

    new commit

--- app/src/main/java/com/example/sampatika/apis/ApiInterface.kt
+++ app/src/main/java/com/example/sampatika/apis/ApiInterface.kt
@@ -1,4 +1,11 @@
 package com.example.sampatika.apis
 
+import com.example.sampatika.fragment.models.MarketModel
+import okhttp3.Response
+import retrofit2.http.GET
+
 interface ApiInterface {
+
+    @GET("data-api/v3/cryptocurrency/listing?start=1&limit=500")
+    suspend fun getMarketData() : Response
 }
