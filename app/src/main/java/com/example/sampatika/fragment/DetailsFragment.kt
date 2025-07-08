7d1c0cb36b22365713230db9333edb5eff2a6365
Vishal Verma <vishalpbh022@gmail.com>
2025-07-08 07:47:59 +0530

    completed detailed char view

--- app/src/main/java/com/example/sampatika/fragment/DetailsFragment.kt
+++ app/src/main/java/com/example/sampatika/fragment/DetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.sampatika.R
@@ -29,8 +30,75 @@ class  DetailsFragment : Fragment() {
 
         loadChart(data)
 
+        setButtonOnClick(data)
+
         return binding.root
     }
+
+    private fun setButtonOnClick(item: CryptoCurrency ) {
+        val oneMonth = binding.button
+        val oneWeek = binding.button
+        val oneDay = binding.button
+        val fourHour = binding.button
+        val oneHour = binding.button
+        val fifteenMinute = binding.button
+
+        val clickListener = View.OnClickListener {
+            when(it.id){
+                fifteenMinute.id -> loadChartData(it, "15", item, oneDay, oneMonth, oneWeek, fourHour, oneHour)
+                oneHour.id -> loadChartData(it, "1H", item, oneDay, oneMonth, oneWeek, fourHour, fifteenMinute)
+                fourHour.id -> loadChartData(it, "4H", item, oneDay, oneMonth, oneWeek, fifteenMinute, oneHour)
+                oneDay.id -> loadChartData(it, "D", item, fifteenMinute, oneMonth, oneWeek, fourHour, oneHour)
+                oneWeek.id -> loadChartData(it, "W", item, oneDay, oneMonth, fifteenMinute, fourHour, oneHour)
+                oneMonth.id -> loadChartData(it, "M", item, oneDay, fifteenMinute, oneWeek, fourHour, oneHour)
+
+            }
+        }
+
+        fifteenMinute.setOnClickListener(clickListener)
+        oneHour.setOnClickListener(clickListener)
+        fourHour.setOnClickListener(clickListener)
+        oneDay.setOnClickListener(clickListener)
+        oneWeek.setOnClickListener(clickListener)
+        oneMonth.setOnClickListener(clickListener)
+    }
+
+    private fun loadChartData(
+        it: View,
+        s: String,
+        item: CryptoCurrency,
+        oneDay: AppCompatButton,
+        oneMonth: AppCompatButton,
+        oneWeek: AppCompatButton,
+        fourHour: AppCompatButton,
+        oneHour: AppCompatButton
+    ) {
+        disableButton(oneDay, oneMonth, oneWeek, fourHour, oneHour)
+        it!!.setBackgroundResource(R.drawable.active_button)
+        binding.detaillChartWebView.settings.javaScriptEnabled = true
+        binding.detaillChartWebView.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+        binding.detaillChartWebView.loadUrl(
+            "https://s.tradingview.com/widgetembed/?frameElementId=tradingview_76d87&symbol=" + item.symbol
+                .toString() + "USD&interval=" +s+"&hidesidetoolbar=1&hidetoptoolbar=1&symboledit=1&saveimage=1&toolbarbg=" +
+                    "F1F3F6&studies=[]&hideideas=1&theme=Dark&style=1&timezone=Etc%2FUTC&studies_overrides={}&overrides={}&enabled_features=" +
+                    "[]&disabled_features=[]&locale=en&utm_source=coinmarketcap.com&utm_medium=widget&utm_campaign=chart&utm_term=BTCUSDT"
+        )
+    }
+    private fun disableButton(
+        onrDay: AppCompatButton,
+        oneMonth: AppCompatButton,
+        oneWeek: AppCompatButton,
+        fourHour: AppCompatButton,
+        oneHour: AppCompatButton
+    ) {
+        onrDay.background = null
+        oneMonth.background = null
+        oneWeek.background = null
+        fourHour.background = null
+        oneHour.background = null
+    }
+
+
 
     private fun loadChart(item: CryptoCurrency) {
         binding.detaillChartWebView.settings.javaScriptEnabled = true
@@ -67,3 +135,5 @@ class  DetailsFragment : Fragment() {
     }
 
 }
+
+
--- app/src/main/java/com/example/sampatika/fragment/models/CryptoCurrency.kt
+++ app/src/main/java/com/example/sampatika/fragment/models/CryptoCurrency.kt
@@ -22,4 +22,15 @@ data class CryptoCurrency(
     val symbol: String,
     val tags: List<String>,
     val totalSupply: Double
-) : Serializable
+) : Serializable{
+    override fun equals(other: Any?): Boolean {
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        return super.hashCode()
+    }
+    override fun toString(): String {
+        return super.toString()
+    }
+}
