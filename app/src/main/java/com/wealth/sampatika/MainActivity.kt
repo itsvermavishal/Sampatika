87d2705ea1379c2d4ac29d3e1e1f697cc5c9f634
Vishal Verma <vishalpbh022@gmail.com>
2026-02-11 07:42:57 +0530

    Added safe top bar and fixed issue

--- app/src/main/java/com/wealth/sampatika/MainActivity.kt
+++ app/src/main/java/com/wealth/sampatika/MainActivity.kt
@@ -3,24 +3,47 @@ package com.wealth.sampatika
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.PopupMenu
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.findNavController
 import com.wealth.sampatika.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 1️⃣ Allow edge-to-edge drawing
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+        // 2️⃣ APPLY SYSTEM INSETS HERE ⬇⬇⬇
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            view.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+
+            insets
+        }
+
+        // 3️⃣ Navigation setup
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
         val navController = navHostFragment!!.findNavController()
 
         val popupMenu = PopupMenu(this, binding.bottomBar)
         popupMenu.inflate(R.menu.bottom_nav_menu)
 
         binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
-
     }
 }
